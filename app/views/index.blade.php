2535df5c5a5aff6d8b8b13fba858bcce1bd5bbac
Paul Day <paul@chuffed.prg>
2014-11-18 17:54:54 +1100

    updated viewport

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
   <title>Compassionate Careers</title>
   <link href="favicon.png" rel="shortcut icon">
   <!-- <link href='https://fonts.googleapis.com/css?family=Roboto|Montserrat:400,700|Open+Sans:400,300,600' rel='stylesheet' type='text/css'> -->
